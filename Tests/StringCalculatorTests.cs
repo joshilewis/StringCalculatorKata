cfbac51e27a5572e0a6cabb2930b39a7683cf505
Joshua Lewis <joshua.lewis@dstvdm.com>
2014-06-18 15:52:28 +0200

    Moved Add() into StringCalculator class in preparation for Interactivity Kata

--- Tests/StringCalculatorTests.cs
+++ Tests/StringCalculatorTests.cs
@@ -7,48 +7,53 @@ using NUnit.Framework;
 namespace Tests
 {
     [TestFixture]
-    public class AddTests
+    public class StringCalculatorTests
     {
         [Test]
         public void AddingEmptyStringShouldReturnZero()
         {
-            Assert.That(Add(""), Is.EqualTo(0));
+            Assert.That(new StringCalculator().Add(""), Is.EqualTo(0));
         }
 
         [Test]
         public void AddingSingleNumberShouldReturnThatNumber()
         {
-            Assert.That(Add("1"), Is.EqualTo(1));
+            Assert.That(new StringCalculator().Add("1"), Is.EqualTo(1));
         }
 
         [Test]
         public void AddingTwoNumbersShouldReturnTheirSum()
         {
-            Assert.That(Add("1, 2"), Is.EqualTo(3));
+            Assert.That(new StringCalculator().Add("1, 2"), Is.EqualTo(3));
         }
 
         [Test]
         public void AddingManyNumbersShouldReturnTheirSum()
         {
-            Assert.That(Add("1, 2, 3, 4"), Is.EqualTo(10));
+            Assert.That(new StringCalculator().Add("1, 2, 3, 4"), Is.EqualTo(10));
         }
 
         [Test]
         public void NewlineCharacterShouldBeTreatedAsDelimiter()
         {
-            Assert.That(Add("1\n2, 3"), Is.EqualTo(6));
+            Assert.That(new StringCalculator().Add("1\n2, 3"), Is.EqualTo(6));
         }
 
+    }
+
+    public class StringCalculator
+    {
         public int Add(string input)
         {
             if (string.IsNullOrEmpty(input))
                 return 0;
+
             return input
                 .Split(',', '\n')
                 .Select(int.Parse)
                 .Sum()
                 ;
-            return int.Parse(input);
         }
+         
     }
 }
